484f3bd6bf9b44780b24dae653c505490b890dfa
SHREYASPATIL2005 <shreyaspatil1408@gmail.com>
2026-06-22 17:39:36 +0530

    Add Chapter 10: Problem 2 - Instance vs Class attributes

---
+++ Chapter 10/02_problem2.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def __init__(self, n):
+        self.n = n  
+
+    def square(self):
+        print(f"The square of {self.n} is {self.n ** 2}")
+
+    def cube(self):
+        print(f"The cube of {self.n} is {self.n ** 3}")
+
+    def squareroot(self):
+        print(f"The square root of {self.n} is {self.n ** 0.5}")
+
+a = Calculator(4)
+a.square()
+a.cube()
+a.squareroot()
