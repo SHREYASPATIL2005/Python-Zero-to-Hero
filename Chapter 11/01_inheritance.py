01dcc0d51b956c5177bc309059537423ebbec189
SHREYASPATIL2005 <shreyaspatil1408@gmail.com>
2026-06-23 17:41:48 +0530

    Chapter 11: add inheritance example

---
+++ Chapter 11/01_inheritance.py
@@ -0,0 +1,26 @@
+class Employee:
+    company = "ITC"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+'''
+class Programmer(Employee):
+    company = "ITC InfoTech"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+    def showlanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language .")
+'''
+class Programmer(Employee):
+    company = "ITC InfoTech"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+    def showlanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language .")
+        
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
