e1d9e0376f184880de442e244af51861d6754d2b
SHREYASPATIL2005 <shreyaspatil1408@gmail.com>
2026-06-28 16:38:33 +0530

    Add Advance_Python_2/04_format.py

---
+++ Advance_Python_2/04_format.py
@@ -0,0 +1,5 @@
+# a = "{} is a good {}".format("Shreyas", "boy")
+# print(a)  # Output: Shreyas is a good
+
+a = "{1} is a good {0}".format("Shreyas", "boy")
+print(a)  # Output: boy is a good Shreyas
